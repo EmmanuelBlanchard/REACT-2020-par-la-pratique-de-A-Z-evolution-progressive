9adf907017559b43e84f88ec9ee2c0b64ad55a86
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-18 21:25:22 +0100

    03-Module03-LesComposants-03-DesComposantsDansDesComposants

--- Programmes/03-Module03-LesComposants/projet0/src/App.js
+++ Programmes/03-Module03-LesComposants/projet0/src/App.js
@@ -1,24 +1,38 @@
 // Importer react pour pouvoir utiliser du code JSX
 // Utilisation du destructuring pour recuperer la partie Component à l'intérieur du module React
 import React, {Component} from 'react';
+import Personne from './Personne';
 
 //Fonction qui retourne du code JSX (Composant App)
-function App() {
-    return (
-        <>
-            <h1>Hello World !</h1>
-            <p>Mon prénom est Matthieu</p>
-        </>   
-    );
-}
+// function App() {
+//     return (
+//         <>
+//             <Personne />
+//             <Personne />
+//             <Personne />
+//         </>
+//     );
+// }
 
 // Transformation de la fonction app() en classe App()
-// extends => donne accès aux attributs et fonctions de la classe "mère" 
+// extends => donne accès aux attributs et fonctions de la classe "mère"
+// Lors de la creation d'une classe, implementer la fonction render pour l'affichage  
 // class App extends Component {
-//     // Lors de la creation d'une classe, implementer la fonction render pour l'affichage 
 //     render() {
 //         return <h1>Hello World !</h1>;
 //     }
 // }
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <Personne />
+                <Personne />
+                <Personne />
+            </>
+        );
+    }
+}
 
 export default App;
---
+++ Programmes/03-Module03-LesComposants/projet0/src/Personne.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function personne() {
+    return (
+        <>
+            <h1>Matthieu</h1>
+            <div>Age : 31</div>
+            <div>Sexe : Homme</div>
+        </>   
+    );
+}
+
+export default personne;
