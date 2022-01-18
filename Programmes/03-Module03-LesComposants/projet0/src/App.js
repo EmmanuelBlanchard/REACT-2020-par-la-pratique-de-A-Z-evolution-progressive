73021e1b8372b949c09140449bbde02c689591e8
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-18 19:39:28 +0100

    03-Module03-LesComposants-01-LesComposantsSimples

---
+++ Programmes/03-Module03-LesComposants/projet0/src/App.js
@@ -0,0 +1,19 @@
+// Importer react pour pouvoir utiliser du code JSX
+// Utilisation du destructuring pour recuperer la partie Component à l'intérieur du module React
+import React, {Component} from 'react';
+
+// Fonction qui retourne du code JSX (Composant App)
+// function app() {
+//     return <h1>Hello World !</h1>;
+// }
+
+// Transformation de la fonction app() en classe App()
+// extends => donne accès aux attributs et fonctions de la classe "mère" 
+class App extends Component {
+    // Lors de la creation d'une classe, implementer la fonction render pour l'affichage 
+    render() {
+        return <h1>Hello World !</h1>;
+    }
+}
+
+export default App;
