37b326277fdec92d6815cd6166b0a72b8ff395c3
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-19 14:21:11 +0100

    04-Module04-LesStates-05-Synthese

--- Programmes/04-Module04-LesStates/projet0/src/Horloge.js
+++ Programmes/04-Module04-LesStates/projet0/src/Horloge.js
@@ -18,16 +18,26 @@ class Horloge extends Component {
         this.setState((oldState,props) => {
             return {
                 date: new Date(),
-                compteur: oldState.compteur + 1
+                compteur: oldState.compteur + 1 // Dépend de l'ancienne valeur
             }
         });
     }
+
+    tick2 = () => {
+        this.setState({
+            date: new Date() // Ne dépend pas de l'ancienne valeur
+        });
+    }
+
     componentDidMount() {
         // Fonction setInterval, pour lancer notre timer
         // Utiliser une fonction de rappel qui sera lancée de manière asynchrone
         // Stocker le timer pour pouvoir l'arrêter => this.timerID
         // this.timerID = setInterval(() => this.setState({date:new Date()}), 1000);
-        this.timerID = setInterval(() => this.tick(), 1000);
+        this.timerID = setInterval(
+            () => this.tick()
+            , 1000
+        );
     }
 
     // Fonction qui se lance au moment du démontage d'un composant
