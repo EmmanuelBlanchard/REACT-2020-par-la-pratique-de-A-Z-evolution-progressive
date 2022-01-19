45e56fa50bfba53331289ef055fd58f299866b60
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-19 14:13:35 +0100

    04-Module04-LesStates-04-setState

--- Programmes/04-Module04-LesStates/projet0/src/Horloge.js
+++ Programmes/04-Module04-LesStates/projet0/src/Horloge.js
@@ -3,22 +3,45 @@ import React, {Component} from "react";
 class Horloge extends Component {
     state = {
         date: new Date(),
+        compteur: 1
     }
 
+    tick = () => {
+        // this.setState({
+        //     date: new Date(),
+        //     compteur: this.state.compteur + 1
+        // });
+        // Fonction réalisée de manière asynchrone, on pourra avoir une certaine incohérence
+        // Dès lors que l'on voudra modifier une valeur dans le state en fonction d'une 
+        // précèdente valeur du state (pas certain que cela se fasse dans le bon ordre) =>
+        // Passer par une fonction qui va modifier le state
+        this.setState((oldState,props) => {
+            return {
+                date: new Date(),
+                compteur: oldState.compteur + 1
+            }
+        });
+    }
     componentDidMount() {
         // Fonction setInterval, pour lancer notre timer
         // Utiliser une fonction de rappel qui sera lancée de manière asynchrone
         // Stocker le timer pour pouvoir l'arrêter => this.timerID
-        this.timerID = setInterval(() => this.setState({date:new Date()}), 1000);
+        // this.timerID = setInterval(() => this.setState({date:new Date()}), 1000);
+        this.timerID = setInterval(() => this.tick(), 1000);
     }
 
-    // Fonction qui se lance au momnent du démontage d'un composant
+    // Fonction qui se lance au moment du démontage d'un composant
     componentWillUnmount() {
         clearInterval(this.timerID);
     }
 
     render(){
-        return <h2>Horloge : {this.state.date.toLocaleTimeString()}</h2>
+        return (
+            <>
+                <h2>Horloge : {this.state.date.toLocaleTimeString()}</h2>
+                <div>Compteur : {this.state.compteur}</div>
+            </>
+        );
     }
 }
 
