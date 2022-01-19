1d8e062108de16a520989699017df979c078dd9f
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-19 14:49:01 +0100

    05-Module05-Structuration(Dossier,CSS ...)-01-RestructurationDuDossierSRC

---
+++ Programmes/05-Module05-Structuration(Dossier,CSS ...)/projet0/src/App.js
@@ -0,0 +1,40 @@
+// Importer react pour pouvoir utiliser du code JSX
+// Utilisation du destructuring pour recuperer la partie Component à l'intérieur du module React
+import React, {Component} from 'react';
+import Personne from './components/Personne/Personne';
+import Horloge from "./containers/Horloge/Horloge";
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <Horloge />
+                <Personne nom="Matthieu" age="31" sexe="Homme"/>
+                <Personne nom="Tya" age="25" sexe="Femme"/>
+                <Personne nom="Milo" age="43" sexe="Homme"/>
+            </>
+        );
+    }
+}
+
+//Fonction qui retourne du code JSX (Composant App)
+// function App() {
+//     return (
+//         <>
+//             <Personne />
+//             <Personne />
+//             <Personne />
+//         </>
+//     );
+// }
+
+// Transformation de la fonction app() en classe App()
+// extends => donne accès aux attributs et fonctions de la classe "mère"
+// Lors de la creation d'une classe, implementer la fonction render pour l'affichage  
+// class App extends Component {
+//     render() {
+//         return <h1>Hello World !</h1>;
+//     }
+// }
+
+export default App;
---
+++ Programmes/05-Module05-Structuration(Dossier,CSS ...)/projet0/src/components/Personne/Personne.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import AgePersonne from "./AgePersonne/AgePersonne";
+
+class Personne extends Component {
+    render() {
+        return (
+            <>
+                <h1>{this.props.nom}</h1>
+                <AgePersonne age={this.props.age}/>
+                <div>Sexe : {this.props.sexe}</div>
+            </>
+        );
+    } 
+}
+
+// Utilisation du système de propriété, props pour indiquer les propriétés que l'on récupère
+// Syntaxe ES6
+// const personne = props => {
+//     return (
+//         <>
+//             <h1>{props.nom}</h1>
+//             <div>Age : {props.age}</div>
+//             <div>Sexe : {props.sexe}</div>
+//         </>   
+//     );
+// }
+
+export default Personne;
