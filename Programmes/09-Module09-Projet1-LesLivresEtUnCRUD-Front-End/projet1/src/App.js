5f99bd864158fcb52dfa8aaa1d6464d9f3c664a4
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-20 21:21:17 +0100

    09-Module09-Projet1-LesLivresEtUnCRUD-Front-End-11-BoutonDAjoutDUnLivre

--- Programmes/09-Module09-Projet1-LesLivresEtUnCRUD-Front-End/projet1/src/App.js
+++ Programmes/09-Module09-Projet1-LesLivresEtUnCRUD-Front-End/projet1/src/App.js
@@ -4,17 +4,26 @@ import Bouton from './components/Bouton/Bouton';
 import Livres from './containers/Livres/Livres';
 
 class App extends Component {
+  state = {
+    ajoutLivre : false
+  }
+
+  handleClicAjoutLivre = () => {
+    this.setState((oldState, props) => {
+      return {ajoutLivre: !oldState.ajoutLivre}
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <TitreH1>
-          Page listant les livres
-        </TitreH1>
+        <TitreH1>Page listant les livres</TitreH1>
         <Livres />
         <Bouton 
           typeBtn="btn-success" 
           css="w-100" 
-          clic={() => console.log("Ajout")}>Ajouter
+          clic={this.handleClicAjoutLivre}>
+          { !this.state.ajoutLivre ? "Ajouter" : "Ferme l'ajout"}
         </Bouton>
       </div>
     );
