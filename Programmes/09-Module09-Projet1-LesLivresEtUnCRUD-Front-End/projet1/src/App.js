c2210f096aabe13f8a97368d46ce18187594537e
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-20 18:07:14 +0100

    09-Module09-Projet1-LesLivresEtUnCRUD-Front-End-06-ComposantBouton

--- Programmes/09-Module09-Projet1-LesLivresEtUnCRUD-Front-End/projet1/src/App.js
+++ Programmes/09-Module09-Projet1-LesLivresEtUnCRUD-Front-End/projet1/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import TitreH1 from './components/Titres/TitreH1';
+import Bouton from './components/Bouton/Bouton';
 
 class App extends Component {
   render() {
@@ -9,7 +10,9 @@ class App extends Component {
           Page listant les livres
         </TitreH1>
         <div>Livres</div>
-        <button>Ajouter</button>
+        <Bouton typeCss="btn-success" clic={ () => console.log("Ajout")}>Ajouter</Bouton>
+        <Bouton typeCss="btn-warning" clic={ () => console.log("Modifier")}>Modification</Bouton>
+        <Bouton typeCss="btn-danger" clic={ () => console.log("Supprimer")}>Suppression</Bouton>
       </div>
     );
   }
