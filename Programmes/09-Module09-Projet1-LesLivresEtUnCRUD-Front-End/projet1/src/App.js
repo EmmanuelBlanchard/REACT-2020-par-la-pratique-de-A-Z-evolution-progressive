a896e2e4b554b2262e39da6fc57d686163967e8d
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-20 16:40:11 +0100

    09-Module09-Projet1-LesLivresEtUnCRUD-Front-End-04-CreerUnSnippet

--- Programmes/09-Module09-Projet1-LesLivresEtUnCRUD-Front-End/projet1/src/App.js
+++ Programmes/09-Module09-Projet1-LesLivresEtUnCRUD-Front-End/projet1/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TitreH1 from './components/Titres/TitreH1';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
       <div className="container">
         <TitreH1>
           Page listant les livres
         </TitreH1>
-        {/* <TitreH1 texte="Page listant les livres" /> */}
         <div>Livres</div>
         <button>Ajouter</button>
       </div>
-  );
+    );
+  }
 }
 
 export default App;
--- Programmes/09-Module09-Projet1-LesLivresEtUnCRUD-Front-End/projet1/src/components/Titres/TitreH1.js
+++ Programmes/09-Module09-Projet1-LesLivresEtUnCRUD-Front-End/projet1/src/components/Titres/TitreH1.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const titreH1 = (props) => (
     <h1 className="border border-dark p-3 m-3 bg-primary text-white text-center rounded">{props.children}</h1>
-    /* <h1 className="border border-dark p-3 m-3 bg-primary text-white text-center rounded">{props.texte}</h1> */
 );
 
 export default titreH1;
