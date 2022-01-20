67f7ce070ae11650746a2dc8545ea8759c07ccc4
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-20 19:16:34 +0100

    09-Module09-Projet1-LesLivresEtUnCRUD-Front-End-08-ListeDeLivres

--- Programmes/09-Module09-Projet1-LesLivresEtUnCRUD-Front-End/projet1/src/containers/Livres/Livres.js
+++ Programmes/09-Module09-Projet1-LesLivresEtUnCRUD-Front-End/projet1/src/containers/Livres/Livres.js
@@ -2,6 +2,15 @@ import React, {Component} from 'react';
 import Bouton from '../../components/Bouton/Bouton';
 
 class Livres extends Component {
+    state = {
+        livres : [
+            {id:1, titre:"L'algorithmique selon H2PROG", auteur:"Matthieu GASTON", nombreDePages:200},
+            {id:2, titre:"La France du 19ème", auteur: "Albert PATRICK", nombreDePages: 500},
+            {id:3, titre:"Le monde des animaux", auteur: "Marc MERLIN", nombreDePages: 250},
+            {id:4, titre:"Le Virus d'Asie", auteur: "Tya MILO", nombreDePages: 120},
+        ]
+    }
+
     render() {
         return (
             <table className="table text-center">
@@ -14,13 +23,19 @@ class Livres extends Component {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Titre 1</td>
-                        <td>Auteur 1</td>
-                        <td>200 pages</td>
-                        <td><Bouton typeBtn="btn-warning" clic={() => console.log("modification")}>Modifier</Bouton></td>
-                        <td><Bouton typeBtn="btn-danger" clic={() => console.log("suppression")}>Supprimer</Bouton></td>
-                    </tr>
+                    {
+                        this.state.livres.map(livre => {
+                            return (
+                                <tr key={livre.id}>
+                                    <td>{livre.titre}</td>
+                                    <td>{livre.auteur}</td>
+                                    <td>{livre.nombreDePages}</td>
+                                    <td><Bouton typeBtn="btn-warning" clic={() => console.log("modification")}>Modifier</Bouton></td>
+                                    <td><Bouton typeBtn="btn-danger" clic={() => console.log("suppression")}>Supprimer</Bouton></td>
+                                </tr>
+                            );
+                        })
+                    }
                 </tbody>
             </table>
         );
