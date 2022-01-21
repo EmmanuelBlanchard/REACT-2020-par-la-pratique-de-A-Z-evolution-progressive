5d198a6ac618dd863f33394868b04fc911928ab9
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-21 23:02:55 +0100

    12-Module12-Projet2-CreateurDePersonnage-AjoutDeFirebase-05-ComposantImagePersonnageAjoutFleches

--- Programmes/12-Module12-Projet2-CreateurDePersonnage-AjoutDeFirebase/projet2/src/containers/CreateurPersonnage/Personnage/ImagePersonnage/ImagePersonnage.js
+++ Programmes/12-Module12-Projet2-CreateurDePersonnage-AjoutDeFirebase/projet2/src/containers/CreateurPersonnage/Personnage/ImagePersonnage/ImagePersonnage.js
@@ -3,6 +3,8 @@ import ImagePersonnage1 from '../../../../assets/images/persos/player1.png';
 import ImagePersonnage2 from '../../../../assets/images/persos/player2.png';
 import ImagePersonnage3 from '../../../../assets/images/persos/player3.png';
 
+import classes from './ImagePersonnage.module.css';
+
 const imagePersonnage = (props) => {
     let imageToPrint="";
     if(props.numImage === 1) imageToPrint = ImagePersonnage1;
@@ -10,8 +12,12 @@ const imagePersonnage = (props) => {
     else if(props.numImage === 3) imageToPrint = ImagePersonnage3;
 
     return (
-        <div>
-            <img src={imageToPrint} alt='perso'/>
+        <div className="row no-gutters text-center align-items-center">
+            <div className={["col-1",classes.fleche,classes.gauche].join(' ')}></div>
+            <div className="col">
+                <img src={imageToPrint} alt='perso'/>
+            </div>
+            <div className={["col-1",classes.fleche,classes.droite].join(' ')}></div> 
         </div>
     );
 };
