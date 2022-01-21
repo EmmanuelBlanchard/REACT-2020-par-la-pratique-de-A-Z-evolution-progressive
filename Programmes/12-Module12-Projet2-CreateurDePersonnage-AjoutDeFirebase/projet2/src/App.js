40a7bc22d537f54ad91b7c85fa1451a35de47f8c
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-21 21:46:58 +0100

    12-Module12-Projet2-CreateurDePersonnage-AjoutDeFirebase-02-ComposantsBoutonEtTitre

--- Programmes/12-Module12-Projet2-CreateurDePersonnage-AjoutDeFirebase/projet2/src/App.js
+++ Programmes/12-Module12-Projet2-CreateurDePersonnage-AjoutDeFirebase/projet2/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import TitreH1 from "./components/Titres/TitreH1";
+import Bouton from "./components/Bouton/Bouton";
 
 function App() {
   return (
     <div className="container">
-      <h1>Créateur de personnage</h1>
+      <TitreH1>Créateur de personnage</TitreH1>
       <div>Personnage</div>
       <div>Armes</div>
-      <button className="btn btn-danger">Réinitialiser</button>
-      <button className="btn btn-success">Créer</button>
+      <div className="row no-gutters">
+        <Bouton typeBtn="btn-danger" css="col-6" clic={() => console.log("Réinitialisation")}>Réinitialiser</Bouton>
+        <Bouton typeBtn="btn-success" css="col-6" clic={() => console.log("Création")}>Créer</Bouton>
+      </div>
     </div>
   );
 }
---
+++ Programmes/12-Module12-Projet2-CreateurDePersonnage-AjoutDeFirebase/projet2/src/components/Titres/TitreH1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './TitreH1.module.css';
+
+const titreH1 = (props) => {
+    const monCss = `${classes.policeTitreH1} border border-dark p-3 m-3 bg-primary text-white text-center rounded`;
+    return (
+        <h1 className={monCss}>{props.children}</h1>
+    );
+};
+
+export default titreH1;
