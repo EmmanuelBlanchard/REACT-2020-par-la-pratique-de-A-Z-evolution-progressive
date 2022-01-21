997cde7b138aba2fd5f2346552f4c7e72a8a1a21
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-21 21:26:49 +0100

    12-Module12-Projet2-CreateurDePersonnage-AjoutDeFirebase-01-PreparationDuProjet

---
+++ Programmes/12-Module12-Projet2-CreateurDePersonnage-AjoutDeFirebase/projet2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Créateur de personnage</h1>
+      <div>Personnage</div>
+      <div>Armes</div>
+      <button className="btn btn-danger">Réinitialiser</button>
+      <button className="btn btn-success">Créer</button>
+    </div>
+  );
+}
+
+export default App;
