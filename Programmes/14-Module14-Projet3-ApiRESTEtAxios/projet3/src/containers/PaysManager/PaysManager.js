84eacc4e4564982135cfae2a9c48ca2a8e205002
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-25 18:04:56 +0100

    14-Module14-Projet3-ApiRESTEtAxios-02-UtilisationDeLApiREST

--- Programmes/14-Module14-Projet3-ApiRESTEtAxios/projet3/src/containers/PaysManager/PaysManager.js
+++ Programmes/14-Module14-Projet3-ApiRESTEtAxios/projet3/src/containers/PaysManager/PaysManager.js
@@ -1,8 +1,44 @@
 import React, {Component} from 'react';
 import TitreH1 from '../../components/Titres/TitreH1';
 import Bouton from '../../components/Bouton/Bouton';
+import axios from 'axios';
+import Pays from './Pays/Pays';
 
 class PaysManager extends Component {
+    state = {
+        listePays: [],
+        loading : false
+    }
+
+    componentDidMount = () => {
+        this.setState({loading:true});
+        // https://restcountries.eu/rest/v2/all // Ne fonctionne pas
+        // https://restcountries.com/v3.1/all
+        // https://restcountries.com/v2/all
+        axios.get("https://restcountries.com/v2/all")
+            .then(reponse => {
+                // console.log(reponse);
+                const listePays = reponse.data.map(pays => {
+                    // console.log(pays);
+                    return {
+                        nom : pays.name,
+                        nomFrancais : pays.translations.fr,
+                        capitale : pays.capital,
+                        region : pays.region,
+                        drapeau : pays.flag
+                    }
+                })
+                this.setState({
+                    listePays,
+                    loading:false
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({loading:false});
+            })
+    }
+
     render() {
         return (
             <div className="container">
@@ -14,6 +50,19 @@ class PaysManager extends Component {
                 <Bouton typeBtn="btn-info">Amérique</Bouton>
                 <Bouton typeBtn="btn-info">Océanie</Bouton>
                 <div>Pays</div>
+                {
+                    this.state.loading 
+                    ? <div>Chargement...</div>
+                    : <div className="row no-gutters">
+                        {this.state.listePays.map(pays => {
+                            return (
+                                <div className="col-12 col-md-6" key={pays.nom}>
+                                    <Pays {...pays}/>
+                                </div>
+                            );
+                        })}
+                    </div>
+                }
                 <div>Pagination</div>
             </div>
         );
