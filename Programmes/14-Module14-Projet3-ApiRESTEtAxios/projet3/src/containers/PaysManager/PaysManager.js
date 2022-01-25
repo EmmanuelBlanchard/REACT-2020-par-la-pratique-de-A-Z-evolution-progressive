eeec02bd9dea454d0731b02354e9be0eff1a71e6
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-25 19:35:28 +0100

    14-Module14-Projet3-ApiRESTEtAxios-03-FiltrerLaListeDesPays(RecuperationParRegion-ComposantBoutonReduireOpaciteDesBoutonsNonSelectionnes)

--- Programmes/14-Module14-Projet3-ApiRESTEtAxios/projet3/src/containers/PaysManager/PaysManager.js
+++ Programmes/14-Module14-Projet3-ApiRESTEtAxios/projet3/src/containers/PaysManager/PaysManager.js
@@ -4,20 +4,41 @@ import Bouton from '../../components/Bouton/Bouton';
 import axios from 'axios';
 import Pays from './Pays/Pays';
 
+/*
+https://gitlab.com/amatos/rest-countries
+
+About this Project
+
+This project is inspired on restcountries.eu by Fayder Florez. Although the original project has now moved to a subscription base API, this project is still Open Source and Free to use.
+
+https://restcountries.eu/rest/v2/all // Ne fonctionne pas
+https://restcountries.com/v3.1/all
+https://restcountries.com/v2/all
+*/
+
 class PaysManager extends Component {
     state = {
         listePays: [],
-        loading : false
+        loading : false,
+        regionSelection: null,
+        nombreDePays: null
     }
 
     componentDidMount = () => {
+        this.handleSelectionPaysParRegion('all')
+    }
+
+    handleSelectionPaysParRegion = (region) => {
+        let param='';
+        if(region === 'all') param=region;
+        else param=`region/${region}`;
+
         this.setState({loading:true});
-        // https://restcountries.eu/rest/v2/all // Ne fonctionne pas
-        // https://restcountries.com/v3.1/all
-        // https://restcountries.com/v2/all
-        axios.get("https://restcountries.com/v2/all")
+        
+        axios.get(`https://restcountries.com/v2/${param}`)
             .then(reponse => {
-                // console.log(reponse);
+                console.log(reponse);
+                const nombreDePays = reponse.data.length;
                 const listePays = reponse.data.map(pays => {
                     // console.log(pays);
                     return {
@@ -25,12 +46,14 @@ class PaysManager extends Component {
                         nomFrancais : pays.translations.fr,
                         capitale : pays.capital,
                         region : pays.region,
-                        drapeau : pays.flag
+                        drapeau : pays.flag,
                     }
                 })
                 this.setState({
                     listePays,
-                    loading:false
+                    loading:false,
+                    regionSelection: region,
+                    nombreDePays : nombreDePays
                 });
             })
             .catch(error => {
@@ -43,13 +66,43 @@ class PaysManager extends Component {
         return (
             <div className="container">
                 <TitreH1>Liste des pays du monde</TitreH1>
-                <Bouton typeBtn="btn-info">Tous</Bouton>
-                <Bouton typeBtn="btn-info">Europe</Bouton>
-                <Bouton typeBtn="btn-info">Afrique</Bouton>
-                <Bouton typeBtn="btn-info">Asie</Bouton>
-                <Bouton typeBtn="btn-info">Amérique</Bouton>
-                <Bouton typeBtn="btn-info">Océanie</Bouton>
-                <div>Pays</div>
+                <Bouton 
+                    typeBtn="btn-info" 
+                    clic={() => this.handleSelectionPaysParRegion("all")}
+                    estSelection={this.state.regionSelection==="all"}
+                    >Tous</Bouton>
+                <Bouton 
+                    typeBtn="btn-info" 
+                    clic={() => this.handleSelectionPaysParRegion("Europe")}
+                    estSelection={this.state.regionSelection==="Europe"}
+                    >Europe</Bouton>
+                <Bouton 
+                    typeBtn="btn-info" 
+                    clic={() => this.handleSelectionPaysParRegion("Africa")}
+                    estSelection={this.state.regionSelection==="Africa"}
+                    >Afrique</Bouton>
+                <Bouton 
+                    typeBtn="btn-info" 
+                    clic={() => this.handleSelectionPaysParRegion("Asia")}
+                    estSelection={this.state.regionSelection==="Asia"}
+                    >Asie</Bouton>
+                <Bouton 
+                    typeBtn="btn-info" 
+                    clic={() => this.handleSelectionPaysParRegion("Americas")}
+                    estSelection={this.state.regionSelection==="Americas"}
+                    >Amérique</Bouton>
+                <Bouton 
+                    typeBtn="btn-info" 
+                    clic={() => this.handleSelectionPaysParRegion("Oceania")}
+                    estSelection={this.state.regionSelection==="Oceania"}
+                    >Océanie</Bouton>
+                <Bouton 
+                    typeBtn="btn-info" 
+                    clic={() => this.handleSelectionPaysParRegion("Polar")}
+                    estSelection={this.state.regionSelection==="Polar"}
+                    >Arctique</Bouton>
+                <div className="d-inline m-3">Nombre de pays : <span className="badge bg-success">{this.state.nombreDePays}</span> 
+                </div>
                 {
                     this.state.loading 
                     ? <div>Chargement...</div>
