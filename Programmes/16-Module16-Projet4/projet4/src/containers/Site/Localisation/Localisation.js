9bf75eb2d20b8c8f0ef54476e481cc2ee63082f9
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-26 23:23:53 +0100

    16-Module16-Projet4-08-PageLocalisation-Etape1

--- Programmes/16-Module16-Projet4/projet4/src/containers/Site/Localisation/Localisation.js
+++ Programmes/16-Module16-Projet4/projet4/src/containers/Site/Localisation/Localisation.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import TitreH1 from '../../../components/UI/Titres/TitreH1';
+import Button from 'react-bootstrap/Button';
 
 class Localisation extends Component {
+    state = {
+        recherche : null
+    }
+
+    rechercheEtablissement = (type) => {
+        axios.get(`https://etablissements-publics.api.gouv.fr/v3/departements/79/${type}`)
+            .then(response => {
+                console.log(response.data);
+                this.setState({recherche:response.data})
+            })
+    }
+
     render() {
         return (
             <>
-                Localisation
+                <TitreH1>Rechercher un établissement : </TitreH1>
+                <Button variant="secondary" className='m-2' onClick={() => this.rechercheEtablissement('mairie')}>Mairie</Button>
+                <Button variant="secondary" className='m-2' onClick={() => this.rechercheEtablissement('commissariat_police')}>Commisariat de police</Button>
+                <Button variant="secondary" className='m-2' onClick={() => this.rechercheEtablissement('pole_emploi')}>Pôle emploi</Button>
+                <Button variant="secondary" className='m-2' onClick={() => this.rechercheEtablissement('prefecture')}>Préfecture</Button>
             </>
         );
     }
