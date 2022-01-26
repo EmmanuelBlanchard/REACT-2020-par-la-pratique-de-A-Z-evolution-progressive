d8670eeaf9ef462b78690405a56dd456db173744
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-26 17:41:14 +0100

    16-Module16-Projet4-03-NavBarAvecReactBootstrapEtReactRouterBootstrap

--- Programmes/16-Module16-Projet4/projet4/src/App.js
+++ Programmes/16-Module16-Projet4/projet4/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Site from "./containers/Site/Site";
 import {BrowserRouter} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- Programmes/16-Module16-Projet4/projet4/src/components/UI/NavBar/NavBar.js
+++ Programmes/16-Module16-Projet4/projet4/src/components/UI/NavBar/NavBar.js
@@ -1,11 +1,30 @@
 import React from "react";
-import {NavLink} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import {LinkContainer} from 'react-router-bootstrap';
+import Container from 'react-bootstrap/Container';
 
 const navBar = (props) => (
     <>
-        <NavLink to="/">Accueil</NavLink>
-        <NavLink to="/localisation">Localisation</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Ariège MGA</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <LinkContainer exact to="/">
+                            <Nav.Link>Accueil</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer exact to="/localisation">
+                            <Nav.Link>Localisation</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer exact to="/contact">
+                            <Nav.Link>Contact</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     </>
 );
 
