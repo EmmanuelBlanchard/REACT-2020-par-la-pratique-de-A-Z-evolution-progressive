dc2a72674328aee1730104afe5bb574b52a3fdb4
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-26 16:13:06 +0100

    16-Module16-Projet4-02-ReactRouter

--- Programmes/16-Module16-Projet4/projet4/src/App.js
+++ Programmes/16-Module16-Projet4/projet4/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Site from "./containers/Site/Site";
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
       <Site />
+    </BrowserRouter>
   );
 }
 
--- Programmes/16-Module16-Projet4/projet4/src/containers/Site/Site.js
+++ Programmes/16-Module16-Projet4/projet4/src/containers/Site/Site.js
@@ -3,15 +3,16 @@ import Accueil from './Accueil/Accueil';
 import Localisation from './Localisation/Localisation.js';
 import Contact from './Contact/Contact';
 import NavBar from '../../components/UI/NavBar/NavBar';
+import {Route} from 'react-router-dom';
 
 class Site extends Component {
     render() {
         return (
             <div>
                 <NavBar />
-                <Accueil />
-                <Localisation />
-                <Contact />
+                <Route path="/" exact render={() => <Accueil />} />
+                <Route path="/localisation" component={() => <Localisation />} />
+                <Route path="/contact" render={() => <Contact />} />
             </div>
         );
     }
