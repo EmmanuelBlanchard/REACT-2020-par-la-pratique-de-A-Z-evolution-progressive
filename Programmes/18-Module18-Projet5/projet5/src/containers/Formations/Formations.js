f7fcc8b2d16472c8e65d45b7e66bda0503e96424
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-27 23:37:22 +0100

    18-Module18-Projet5-02-ListerLesFormations

--- Programmes/18-Module18-Projet5/projet5/src/containers/Formations/Formations.js
+++ Programmes/18-Module18-Projet5/projet5/src/containers/Formations/Formations.js
@@ -1,7 +1,22 @@
 import TitreH1 from '../../components/Titres/TitreH1';
 import Bouton from '../../components/Bouton/Bouton';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Formations() {
+    const [formations,setFormations] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://dev.h2prog.com/API_TEST/formations')
+        .then(reponse => {
+            console.log(reponse.data);
+            setFormations(reponse.data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    },[]);
+
     return (
         <div>
             <TitreH1>Bienvenue sur le site listant les formations H2PROG</TitreH1> 
@@ -9,6 +24,35 @@ function Formations() {
             <Bouton typeBtn="btn-outline-secondary" css="m-2">PHP</Bouton>
             <Bouton typeBtn="btn-outline-secondary" css="m-2">JavaScript</Bouton>
             <Bouton typeBtn="btn-outline-secondary" css="m-2">Algorithmique</Bouton>
+
+            <table className='table'>
+                <thead>
+                    <th>Numéro</th>
+                    <th>Libellé</th>
+                    <th>Description</th>
+                    <th>Catégorie</th>
+                    <th>Image</th>
+                </thead>
+                <tbody>
+                    {
+                        formations && formations.map(formation => {
+                            console.log(formation);
+                            return (
+                                <tr key={formation.id}>
+                                    <td>{formation.id}</td>
+                                    <td>{formation.libelle}</td>
+                                    <td>{formation.description}</td>
+                                    <td>{formation.categorie}</td>
+                                    <td>
+                                        <img src={formation.image} className="img-thumbnail" width="200px" alt={formation.image} />
+                                    
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
     );
 }
