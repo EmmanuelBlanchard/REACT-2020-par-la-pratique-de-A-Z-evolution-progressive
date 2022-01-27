5f71a89078d5a9e30220f81301bda68b8c7fe4df
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-01-27 21:07:00 +0100

    17-Module17-IntroductionAuxHooks-03-useEffect

--- Programmes/17-Module17-IntroductionAuxHooks/projet-introduction-hooks/src/containers/Test.js
+++ Programmes/17-Module17-IntroductionAuxHooks/projet-introduction-hooks/src/containers/Test.js
@@ -1,71 +1,77 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Travail avec des hooks
 function Test() {
-    // Conserver un entier, booleen, chaine de caracteres 
-    
+    const [compteur,setCompteur] = useState(0);
     // useState(31); 
     // Retourne la valeur dans la premiere case du tableau et dans la deuxieme case du tableau, elle retourne une fonction pour modifier la valeur
 
     // Utilisation du destructuring js, Declaration des états locals
-    // const [age,setAge] = useState(31); 
-    // const [sexe,setSexe] = useState(true); 
-    // const [couleur,setCouleur] = useState("noire"); 
-    // const anniversaire = () => setAge(age => age + 1);
-    // const changementSexe = () => setSexe(sexe => !sexe);
-    
-    const [personne,setPersonne] = useState({
-        age : 31,
-        sexe: true,
-        couleur: "noire"
-    })
+    const [age,setAge] = useState(31); 
+    const [sexe,setSexe] = useState(true); 
+    const [couleur,setCouleur] = useState("noire"); 
+    const anniversaire = () => setAge(age => age + 1);
+    const changementSexe = () => setSexe(sexe => !sexe);
 
-    const anniversaire = () => {
-        const nouvellePersonne = {...personne};
-        nouvellePersonne.age ++;
-        setPersonne(nouvellePersonne);
-    }
+    // la fonction 'document.title = `L'age est de ${age}`;' va etre réalisée au montage du composant
+    // Pareil que la fonction componentDidMount
+    /*
+    useEffect(() => {
+        document.title = `L'age est de ${age}`;
+    },[]);
+    */
 
-    const changementSexe = () => {
-        const nouvellePersonne = {...personne};
-        nouvellePersonne.sexe = !nouvellePersonne.sexe;
-        setPersonne(nouvellePersonne);
-    }
+    // La fonction sera réalisée tout le temps au montage et à l'update 
+    // Pareil que la componentDidMount et componentDidUpdate
+    /*
+    useEffect(() => {
+        document.title = `L'age est de ${age}`;
+        console.log("ici");
+    });
+    */
 
-    const choixCouleur = (couleurDemandee) => {
-        const nouvellePersonne = {...personne};
-        nouvellePersonne.couleur = couleurDemandee;
-        setPersonne(nouvellePersonne);
-    }
+    // Indique la dépendance age
+    useEffect(() => {
+        document.title = `L'age est de ${age}`;
+        console.log("ici");
+    },[age]);
 
-    // return (
-    //     <div>
-    //         <div>
-    //             Age : {age}
-    //             <button onClick={anniversaire}>Anniversaire</button>
-    //         </div>
-    //         <div>
-    //             <button onClick={changementSexe}>{sexe ? "Homme" : "Femme"}</button>
-    //         </div>
-    //         <div>
-    //             Couleur : 
-    //             <input type="text" value={couleur} onChange={(event) => setCouleur(event.target.value)} />
-    //         </div>
-    //     </div>
-    // );
+    // Pareil que la fonction componentDidMount
+    /*
+    useEffect(() => {
+        setInterval(() => {
+            setCompteur(compteur => compteur + 1);
+        },1000);
+    },[]);
+    */
+
+    // ComponentWillUnmount
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCompteur(compteur => compteur + 1);
+        },1000);
+        return function() { // fonction appelée au démontage
+            clearInterval(timer);
+        }
+    },[]);
+
+    //React recommande de rajouter un useEffect par fonctionnalité différente
 
     return (
         <div>
             <div>
-                Age : {personne.age}
+                Compteur : {compteur} 
+            </div>
+            <div>
+                Age : {age}
                 <button onClick={anniversaire}>Anniversaire</button>
             </div>
             <div>
-                <button onClick={changementSexe}>{personne.sexe ? "Homme" : "Femme"}</button>
+                <button onClick={changementSexe}>{sexe ? "Homme" : "Femme"}</button>
             </div>
             <div>
                 Couleur : 
-                <input type="text" value={personne.couleur} onChange={(event) => choixCouleur(event.target.value)} />
+                <input type="text" value={couleur} onChange={(event) => setCouleur(event.target.value)} />
             </div>
         </div>
     );
